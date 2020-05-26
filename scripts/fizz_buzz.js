0023e2514324e3052e4588f1a48679f9922b88da
Stewart MacLeman <stewart_macleman@hotmail.com>
2020-05-26 16:27:17 +0100

    Basic html and css done.

--- scripts/fizz_buzz.js
+++ scripts/fizz_buzz.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("I'm here!");
+
+let num_display = document.querySelector("#num_display");
+
+let fizz_btn = document.querySelector("#fizz_btn");
+let buzz_btn = document.querySelector("#buzz_btn");
+let fizz_buzz_btn = document.querySelector("#fizz_buzz_btn");
+
+let answer_display = document.querySelector("#answer_display");
+
+let count_start = document.querySelector("#count_start");
+let random_start = document.querySelector("#random_start");
+let reset_btn = document.querySelector("#reset_btn");
