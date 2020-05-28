e783cd460f916d477cd6f946d49bee70aaf6c04d
Stewart MacLeman <stewart_macleman@hotmail.com>
2020-05-28 10:42:55 +0100

    Function names updated, css updates.

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ This application is a version of the game Fizz/Buzz, using HTML, CSS and JavaScr
 
 ## How to use:
 
-* Select the type of game, counting or random....
+* Select the type of game - "Counter Game" or "Random Game". Then click Begin!
+* Click the "Reset Games" button to start again.
--- scripts/fizz_buzz.js
+++ scripts/fizz_buzz.js
@@ -8,8 +8,8 @@ let buzz_btn = document.querySelector("#buzz_btn");
 let fizz_buzz_btn = document.querySelector("#fizz_buzz_btn");
 
 let answer_display = document.querySelector("#answer_display");
+let begin_btn = document.querySelector("#begin_btn");
 let next_btn = document.querySelector("#next_btn");
-let next_btn_2 = document.querySelector("#next_btn_2");
 
 let count_start = document.querySelector("#count_start");
 let random_start = document.querySelector("#random_start");
@@ -19,29 +19,29 @@ let number = 0;
 let submitted_answer = "";
 let game_type = "";
 
-// Answer functions ------------------------------------------------
+// Answer functions -----------------------------------------------------------
 none_btn.addEventListener( "click", answer_none );
 function answer_none(){
   submitted_answer = "none";
-  next_number();
+  game();
 }
 
 fizz_btn.addEventListener( "click", answer_fizz );
 function answer_fizz(){
   submitted_answer = "fizz";
-  next_number();
+  game();
 }
 
 buzz_btn.addEventListener( "click", answer_buzz );
 function answer_buzz(){
   submitted_answer = "buzz";
-  next_number();
+  game();
 }
 
 fizz_buzz_btn.addEventListener( "click", answer_fizz_buzz);
 function answer_fizz_buzz(){
   submitted_answer = "fizz_buzz";
-  next_number();
+  game();
 }
 
 function answer_btns_on(){
@@ -57,32 +57,31 @@ function answer_btns_off(){
   fizz_buzz_btn.disabled = true;
 }
 
-// Counting numbers game --------------------------------------------
+// Counting numbers game (Start Function) --------------------------------------
 count_start.addEventListener( "click", count_game );
 
 function count_game(){
   game_type = "counter";
-  answer_display.innerHTML = "Click \"Begin!\" to start!";
-  next_btn.disabled = false;
-  next_btn.innerHTML = "Begin!";
+  answer_display.innerHTML = "Click \"Begin\" to start!";
+  begin_btn.disabled = false;
+  begin_btn.innerHTML = "Begin!";
   random_start.disabled = true;
 }
-
-// Random numbers game ------------------------------------------------------
+// Random numbers game (Start Function) ----------------------------------------
 random_start.addEventListener( "click", random_game );
 
 function random_game(){
   game_type = "random";
-  answer_display.innerHTML = "Click \"Begin!\" to start!";
-  next_btn.disabled = false;
-  next_btn.innerHTML = "Begin!";
+  answer_display.innerHTML = "Click \"Begin\" to start!";
+  begin_btn.disabled = false;
+  begin_btn.innerHTML = "Begin!";
   count_start.disabled = true;
 }
-// Next Number Function 2---------------------------------------------------
-function next_number_2() {
-  next_btn.style.display = "none";
-  next_btn_2.style.display = "block";
-  next_btn_2.addEventListener( "click", nextNumber );
+// Next Number Function --------------------------------------------------------
+function next_number() {
+  begin_btn.style.display = "none";
+  next_btn.style.display = "block";
+  next_btn.addEventListener( "click", nextNumber );
   function nextNumber(){
     if (game_type == "counter"){
       num_display.innerHTML = number;
@@ -95,20 +94,19 @@ function next_number_2() {
       answer_btns_on();
     }
   }
-
 }
-// Next Number --------------------------------------------------------------
-next_btn.addEventListener("click", next_number);
+// Game Function --------------------------------------------------------------
+begin_btn.addEventListener("click", game);
 
-function next_number(){
+function game(){
 
     if ( (game_type == "counter") && (number == 0) && (submitted_answer == "") ){
       number += 1;
       num_display.innerHTML = number;
       answer_display.innerHTML = "Click one of the answer buttons above.";
       answer_btns_on();
-      next_btn.disabled = true;
-      next_btn.innerHTML = "-";
+      begin_btn.disabled = true;
+      begin_btn.innerHTML = "-";
       count_start.disabled = true;
 
     } else if ( (game_type == "random") && (number == 0) && (submitted_answer == "") ){
@@ -116,8 +114,8 @@ function next_number(){
       num_display.innerHTML = number;
       answer_display.innerHTML = "Click one of the answer buttons above.";
       answer_btns_on();
-      next_btn.disabled = true;
-      next_btn.innerHTML = "-";
+      begin_btn.disabled = true;
+      begin_btn.innerHTML = "-";
       random_start.disabled = true;
 
     } else if ( (submitted_answer == "none") && (number >= 1 ) && (number%3 != 0)
@@ -125,129 +123,128 @@ function next_number(){
       answer_display.innerHTML = "Correct! The number " + number + " can\'t be equally divided by 3 or 5! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "none") && (number >= 1 ) && (number%3 == 0)
     && (number%5 != 0) ){
       answer_display.innerHTML = "Sorry! The number " + number + " can be equally divided by 3, but not 5! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "none") && (number >= 1 ) && (number%3 != 0)
     && (number%5 == 0) ){
       answer_display.innerHTML = "Sorry! The number " + number + " can be equally divided by 5, but not 3! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "none") && (number >= 1 ) && ((number%3 == 0)
     || (number%5 == 0)) ){
       answer_display.innerHTML = "Sorry! The number " + number + " can be equally divided by 3 and 5! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "fizz") && (number >= 1 ) && (number%3 == 0)
     && (number%5 != 0) ){
       answer_display.innerHTML = "Correct! The number " + number + " can be equally divided by 3! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "fizz") && (number >= 1 ) && (number%3 != 0)
     && (number%5 != 0) ){
       answer_display.innerHTML = "Sorry! The number " + number + " can\'t be equally divided by 3! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "fizz") && (number >= 1 ) && (number%3 != 0)
     && (number%5 == 0) ){
       answer_display.innerHTML = "Sorry! The number " + number + " can\'t be equally divided by 3! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "fizz") && (number >= 1 ) && ((number%3 == 0)
     || (number%5 == 0)) ){
       answer_display.innerHTML = "Sorry! The number " + number + " can also be equally divided by 5! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "buzz") && (number >= 1 ) && (number%3 != 0)
     && (number%5 == 0) ){
       answer_display.innerHTML = "Correct! The number " + number + " can be equally divided by 5! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "buzz") && (number >= 1 ) && (number%3 == 0)
     && (number%5 != 0) ){
       answer_display.innerHTML = "Sorry! The number " + number + " can\'t be equally divided by 5! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "buzz") && (number >= 1 ) && (number%3 != 0)
     && (number%5 != 0) ){
       answer_display.innerHTML = "Sorry! The number " + number + " can\'t be equally divided by 5! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "buzz") && (number >= 1 ) && ((number%3 == 0)
     || (number%5 == 0)) ){
       answer_display.innerHTML = "Sorry! The number " + number + " can also be equally divided by 3! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "fizz_buzz") && (number >= 1 ) && (number%3 == 0)
     && (number%5 == 0) ){
       answer_display.innerHTML = "Correct! The number " + number + " can be equally divided by 3 and 5! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "fizz_buzz") && (number >= 1 ) && (number%3 == 0)
     && (number%5 != 0) ){
       answer_display.innerHTML = "Sorry! The number " + number + " can\'t be equally divided by 5! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "fizz_buzz") && (number >= 1 ) && (number%3 != 0)
     && (number%5 == 0) ){
       answer_display.innerHTML = "Sorry! The number " + number + " can\'t be equally divided by 3! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
 
     } else if ( (submitted_answer == "fizz_buzz") && (number >= 1 ) && (number%3 != 0)
     && (number%5 != 0) ){
       answer_display.innerHTML = "Sorry! The number " + number + " can\'t be equally divided by 3 or 5! Now click \"Next Number\".";
       number +=1;
       answer_btns_off();
-      next_number_2();
+      next_number();
     }
 }
-
-// Reseting the game -------------------------------------------------------
+// Reset Function---------------------------------------------------------------
 reset_btn.addEventListener( "click", reset_game );
 
 function reset_game(){
   num_display.innerHTML = "Number Display!"
   answer_display.innerHTML = "Choose a game type from below!"
-  next_btn.innerHTML = "-";
+  begin_btn.innerHTML = "-";
   number = 0;
   submitted_answer = "";
   game_type = "";
-  next_btn.disabled = true;
+  begin_btn.disabled = true;
   random_start.disabled = false;
   count_start.disabled = false;
   answer_btns_off();
-  next_btn.style.display = "block";
-  next_btn_2.style.display = "none";
+  begin_btn.style.display = "block";
+  next_btn.style.display = "none";
 }
