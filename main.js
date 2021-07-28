e2d1d9d339d6e522a1f835e9f16c0c1d7340ec9f
Stewart MacLeman <stewart_macleman@hotmail.com>
2021-07-28 20:25:54 +0100

    Header drop-down functionality added.

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 "use strict";
+// Global variables for the drop-down rules section. -------------------
+let showHowToButton = document.getElementById("howToShow");
+let hideHowToButton = document.getElementById("howToHide");
+let howToDiv = document.getElementById("howToCont");
+
+// Functions to show and remove the rules div. --------------------------
+showHowToButton.addEventListener("click", displayHowTo);
+hideHowToButton.addEventListener("click", removeHowTo);
+
+function displayHowTo(){
+  howToDiv.classList.remove("hide");
+  howToDiv.classList.add("howToContainer");
+  showHowToButton.classList.add("hide");
+}
+function removeHowTo(){
+  howToDiv.classList.remove("howToContainer");
+  howToDiv.classList.add("hide");
+  showHowToButton.classList.remove("hide");
+}
