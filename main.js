1947d92fa375cb8da4cae5ffa8008dcc05b72ed9
Stewart MacLeman <stewart_macleman@hotmail.com>
2021-07-28 21:53:44 +0100

    Initial game selection and reset functions added.

--- main.js
+++ main.js
@@ -18,3 +18,42 @@ function removeHowTo(){
   howToDiv.classList.add("hide");
   showHowToButton.classList.remove("hide");
 }
+// /////////////////////////////////////////////////////////////////////////
+// Global variables for the game funtionality. ---------------------------
+let messageDisplay = document.getElementById("messDis");
+let numberDisplay = document.getElementById("numDis");
+
+let gameSelectionDiv = document.getElementById("gameSelcCon");
+let normalGameButton = document.getElementById("normBut");
+let randomGameButton = document.getElementById("randBut");
+let gameType = "";
+
+let resetButton = document.getElementById("reset");
+
+// Game selection functions. -----------------------------
+normalGameButton.addEventListener("click", selectGame);
+randomGameButton.addEventListener("click", selectGame);
+
+function selectGame(e){
+  let gameValue = e.target.value;
+  gameType = gameValue;
+  console.log("The game type is: " + gameType);
+
+    gameSelectionDiv.classList.add("hide");
+    messageDisplay.classList.remove("hide");
+    numberDisplay.classList.remove("hide");
+    resetButton.classList.remove("hide");
+}
+
+// Reset the game function. ----------------------------
+resetButton.addEventListener("click", resetGame);
+
+function resetGame(){
+  gameType = "";
+
+  gameSelectionDiv.classList.remove("hide");
+  messageDisplay.classList.add("hide");
+  numberDisplay.classList.add("hide");
+  resetButton.classList.add("hide");
+
+}
