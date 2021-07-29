75ca8bdb04c0b9c92f27d59bbc333b1da865d0d7
Stewart MacLeman <stewart_macleman@hotmail.com>
2021-07-29 21:42:12 +0100

    Game functionality added.

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 "use strict";
-// Global variables for the drop-down rules section. -------------------
+// Global variables for the drop-down div section. -------------------
 let showHowToButton = document.getElementById("howToShow");
 let hideHowToButton = document.getElementById("howToHide");
 let howToDiv = document.getElementById("howToCont");
 
-// Functions to show and remove the rules div. --------------------------
+// Functions to show and remove the drop-down div. ------------------
 showHowToButton.addEventListener("click", displayHowTo);
 hideHowToButton.addEventListener("click", removeHowTo);
 
@@ -19,15 +19,18 @@ function removeHowTo(){
   showHowToButton.classList.remove("hide");
 }
 // /////////////////////////////////////////////////////////////////////////
+
 // Global variables for the game funtionality. ---------------------------
 let messageDisplay = document.getElementById("messDis");
 let numberDisplay = document.getElementById("numDis");
+let onScreenNumber = document.getElementById("num");
 
 let gameSelectionDiv = document.getElementById("gameSelcCon");
 let normalGameButton = document.getElementById("normBut");
 let randomGameButton = document.getElementById("randBut");
 let gameType = "";
 let number = 0;
+let submitted_answer = "";
 
 let answerButtonsDiv = document.getElementById("answerSelcCon");
 let fizzButton = document.getElementById("fizzBut");
@@ -39,58 +42,131 @@ let noneButton = document.getElementById("noneBut");
 let playButton = document.getElementById("play");
 let resetButton = document.getElementById("reset");
 
-// Game selection functions. -----------------------------
+// Game selection function. -----------------------------
 normalGameButton.addEventListener("click", selectGame);
 randomGameButton.addEventListener("click", selectGame);
 
 function selectGame(e){
   let gameValue = e.target.value;
   gameType = gameValue;
-  console.log("The game type is: " + gameType);
 
-    gameSelectionDiv.classList.add("hide");
-    messageDisplay.classList.remove("hide");
-    numberDisplay.classList.remove("hide");
+  gameSelectionDiv.classList.add("hide");
+  messageDisplay.classList.remove("hide");
+  messageDisplay.textContent = "Click the Start button to begin.";
+  numberDisplay.classList.remove("hide");
+  onScreenNumber.textContent = "_";
 
+  playButton.classList.remove("hide");
+  resetButton.classList.remove("hide");
+}
+
+// Main game playing functions. ---------------------------------------
+playButton.addEventListener("click", startGame);
+// Submits the answers. ------------------------------------------------
+fizzButton.addEventListener("click", answerSubmitted);
+buzzButton.addEventListener("click", answerSubmitted);
+fizzBuzzButton.addEventListener("click", answerSubmitted);
+noneButton.addEventListener("click", answerSubmitted);
+
+function answerSubmitted(e){
+  let buttonClicked = e.target.value;
+  submitted_answer = buttonClicked;
+  gameLogic();
+}
+// Hiding the submit buttons.---------------------------------
+function hideSubmitAnswerButtons(){
+  answerButtonsDiv.classList.add("hide");
+  answerButtonsDiv.classList.remove("answerSelectonCont");
+}
+// Disables / enables the "Next Number" button.----------------
+function playButtonOff(){
+  playButton.disabled = true;
+  playButton.classList.add("disabledButton");
+}
+function playButtonOn(){
+  playButton.disabled = false;
+  playButton.classList.remove("disabledButton");
+}
+// Generates / updates the numbers. -------------------------------------
+function generateNumber(){
+  if (gameType == "normal"){
+    number += 1;
+    onScreenNumber.innerHTML = number;
+    messageDisplay.textContent = "Click one of the Answer buttons below."
+    playButtonOff();
+  } else if (gameType == "random") {
+    number = Math.floor((Math.random() * 100) + 1);
+    onScreenNumber.innerHTML = number;
+    messageDisplay.textContent = "Click one of the Answer buttons below."
+    playButtonOff();
+  }
+}
+// Starts the game / plays another round. -------------------------------
+function startGame(){
     answerButtonsDiv.classList.remove("hide");
     answerButtonsDiv.classList.add("answerSelectonCont");
-
-    playButton.classList.remove("hide");
-    resetButton.classList.remove("hide");
+    playButton.textContent = "Next Number";
+    generateNumber();
 }
-// Answer buttons functions. --------------------------
-fizzButton.addEventListener("click", testFunction);
-buzzButton.addEventListener("click", testFunction);
-fizzBuzzButton.addEventListener("click", testFunction);
-noneButton.addEventListener("click", testFunction);
+// Evaluates the anwsers. -----------------------------------------------
+function gameLogic(){
+  if (submitted_answer == "none"){
+    if ( (number%3 != 0) && (number%5 != 0) ) {
+      messageDisplay.textContent = `Correct! The number ${number} can't be equally divided by 3 or 5! Click "Next Number" to go again!`;
+      playButtonOn();
+      hideSubmitAnswerButtons();
+    } else {
+      messageDisplay.textContent = `Sorry, try that one again!`;
+    }
 
-function testFunction(e){
-  let buttonText = e.target.textContent;
-  console.log(buttonText);
+  } else if (submitted_answer == "fizz"){
+      if ( (number%3 == 0) && (number%5 != 0) ) {
+        messageDisplay.textContent = `Correct! The number ${number} can be equally  divided by 3, but not 5! Click "Next Number" to go again!`;
+        playButtonOn();
+        hideSubmitAnswerButtons();
+      } else {
+        messageDisplay.textContent = `Sorry, try that one again!`;
+      }
+
+  } else if (submitted_answer == "buzz"){
+      if ( (number%3 != 0) && (number%5 == 0) ) {
+        messageDisplay.textContent = `Correct! The number ${number} can be equally  divided by 5, but not 3! Click "Next Number" to go again!`;
+        playButtonOn();
+        hideSubmitAnswerButtons();
+      } else {
+        messageDisplay.textContent = `Sorry, try that one again!`;
+      }
+
+  } else if (submitted_answer == "fizz_buzz"){
+      if ( (number%3 == 0) && (number%5 == 0) ) {
+        messageDisplay.textContent = `Correct! The number ${number} can be equally  divided by 3 and 5! Click "Next Number" to go again!`;
+        playButtonOn();
+        hideSubmitAnswerButtons();
+      } else {
+        messageDisplay.textContent = `Sorry, try that one again!`;
+      }
+  }
 }
-// Main game playing functions. ------------------------
-playButton.addEventListener("click", startGame);
 
-function startGame(e){
-  let buttonText = e.target.textContent;
-  console.log(buttonText);
-}
-
-// Reset the game function. ----------------------------
+// Reset the game. --------------------------------------------------
 resetButton.addEventListener("click", resetGame);
 
 function resetGame(){
   gameType = "";
   number = 0;
+  submitted_answer = "";
 
   gameSelectionDiv.classList.remove("hide");
   messageDisplay.classList.add("hide");
+  messageDisplay.textContent = "";
   numberDisplay.classList.add("hide");
+  onScreenNumber.textContent = "_";
 
   answerButtonsDiv.classList.add("hide");
   answerButtonsDiv.classList.remove("choiceSelectonCont");
 
+  playButtonOn();
   playButton.classList.add("hide");
+  playButton.textContent = "Start";
   resetButton.classList.add("hide");
-
 }
